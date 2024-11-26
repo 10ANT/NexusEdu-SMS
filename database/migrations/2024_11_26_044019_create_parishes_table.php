275944c453af6f1c2fb2fa9acfd47f8b91f1cb7e
10ANT <tnt528722@gmail.com>
2024-11-26 00:03:19 -0500

    small adjustment to parishes seeder

--- database/migrations/2024_11_26_044019_create_parishes_table.php
+++ database/migrations/2024_11_26_044019_create_parishes_table.php
@@ -11,7 +11,7 @@ class CreateParishesTable extends Migration
         Schema::create('parishes', function (Blueprint $table) {
             $table->id(); // Automatically creates an ID field
             $table->string('name'); // To store the parish name
-            $table->unsignedBigInteger('region_id'); // Foreign key for region
+            //$table->unsignedBigInteger('region_id'); // Foreign key for region
             $table->timestamps(); // Created and updated timestamps
             
             // Adding the foreign key constraint
